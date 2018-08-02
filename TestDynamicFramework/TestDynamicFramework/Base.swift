e7875cde800e2e5d40c265b26d847ab56e0c7e96
mudit jain <mudit.jain@lmsin.com>
2018-08-02 16:58:29 +0530

    addde

---
+++ TestDynamicFramework/TestDynamicFramework/Base.swift
@@ -0,0 +1,21 @@
+//
+//  Base.swift
+//  TestDynamicFramework
+//
+//  Created by LMS on 02/08/18.
+//  Copyright © 2018 LMS. All rights reserved.
+//
+
+import Foundation
+
+public class Base {
+    private init() {
+        
+    }
+    
+    static func myMethod()->String
+    {
+        return "dynamic framework"
+    }
+    
+}
---
+++ TestDynamicFramework/TestDynamicFramework/TestDynamicFramework.h
@@ -0,0 +1,19 @@
+//
+//  TestDynamicFramework.h
+//  TestDynamicFramework
+//
+//  Created by LMS on 02/08/18.
+//  Copyright © 2018 LMS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestDynamicFramework.
+FOUNDATION_EXPORT double TestDynamicFrameworkVersionNumber;
+
+//! Project version string for TestDynamicFramework.
+FOUNDATION_EXPORT const unsigned char TestDynamicFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestDynamicFramework/PublicHeader.h>
+
+
